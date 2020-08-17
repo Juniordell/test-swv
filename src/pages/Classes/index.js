0e8b98a7b78bfee9b572bcf0bbd131ed02004502
Nelson Jr <juniordell123@gmail.com>
2020-08-17 16:18:49 -0300

    add Classes page

---
+++ src/pages/Classes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from 'react-bootstrap'
+
+// import { Container } from './styles';
+
+function Classes() {
+  return (
+      <Container>
+      </Container>
+  );
+}
+
+export default Classes;
