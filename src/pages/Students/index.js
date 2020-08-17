44fa58474ba4fecd991a7a560fb1b0f200d4fc1e
Nelson Jr <juniordell123@gmail.com>
2020-08-17 16:18:31 -0300

    add Students page

---
+++ src/pages/Students/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from 'react-bootstrap'
+
+// import { Container } from './styles';
+
+function Students() {
+  return (
+      <Container>
+      </Container>
+  );
+}
+
+export default Students;
