4f8020c6d9fce41c967bb7e0224f6f5806b4086e
Nelson Jr <juniordell123@gmail.com>
2020-08-18 18:30:08 -0300

    add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Footer() {
+  return (
+      <div style={{margin: 8}}>
+          <hr/>
+
+          <p>@SWV Software 2020 - Todos os direitos reservados</p>
+      </div>
+  );
+}
+
+export default Footer;
